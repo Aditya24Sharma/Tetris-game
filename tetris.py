23219909d248164242689c69cc9270abe211aeea
Aditya Sharma <aditya2002.05.19@gmail.com>
2024-05-16 22:00:14 -0400

    file distribution

---
+++ tetris.py
@@ -0,0 +1,102 @@
+import pygame
+import sys
+from variables import *
+from classes import Shape, Tetris
+
+pygame.init()
+
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main():
+    tetris = Tetris(ROWS, COLS)
+    clock = pygame.time.Clock()
+    counter = 0
+    move = True
+    run = True
+    space_pressed = False
+    while run:
+        SCREEN.fill(BG_COLOR)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                sys.exit()
+        
+        keys = pygame.key.get_pressed()
+        if not tetris.end:
+            if keys[pygame.K_LEFT]:
+                tetris.move_left()
+            elif keys[pygame.K_RIGHT]:
+                tetris.move_right()
+            elif keys[pygame.K_DOWN]:
+                tetris.move_down()
+            elif keys[pygame.K_UP]:
+                tetris.rotate()
+            elif keys[pygame.K_SPACE]:
+                space_pressed = True
+        if keys[pygame.K_r]:
+            tetris.__init__(ROWS, COLS)
+        if keys[pygame.K_ESCAPE] or keys[pygame.K_q]:
+                run = False 
+
+        tetris.make_grid()
+
+        #show shape on the screen
+        if tetris.figure:
+            for i in range(4):
+                for j in range(4):
+                    if(i*4 + j) in tetris.figure.image():
+                        shape = ASSETS[tetris.figure.color]
+                        x = CELL * (tetris.figure.x + j)
+                        y = CELL * (tetris.figure.y + i)
+                        SCREEN.blit(shape, (x,y))
+                        pygame.draw.rect(SCREEN, WHITE, (x,y,CELL, CELL), 1)
+        
+        counter += 1
+        if counter >= 15000:
+            counter = 0
+
+        if move:
+            if counter % (FPS//(tetris.level*2)) == 0:
+                if not tetris.end:
+                    if space_pressed:
+                        tetris.freefall()
+                        space_pressed = False
+                    else: 
+                        tetris.move_down()
+
+        #keep fallen shapes on screen
+        for x in range(ROWS):
+            for y in range(COLS):
+                if tetris.grid[x][y] > 0:
+                    value = tetris.grid[x][y]
+                    image = ASSETS[value]
+                    SCREEN.blit(image, (y*CELL, x*CELL))
+                    pygame.draw.rect(SCREEN, WHITE, (y*CELL,x*CELL, CELL, CELL), 1)
+
+        #Control Panel
+        if tetris.next:
+            for i in range(4):
+                for j in range(4):
+                    if(i*4 + j) in tetris.next.image():
+                        image = ASSETS[tetris.next.color]
+                        x = CELL * (tetris.next.x + j - 4)
+                        y = HEIGHT- 100 + CELL * (tetris.next.y + i)
+                        SCREEN.blit(image, (x,y))
+
+        if tetris.end:
+            tetris.end_game()
+
+
+        score_text = font.render(f'{tetris.score}', True, WHITE)
+        level_text = font2.render(f'Level: {tetris.level}', True, WHITE)
+        SCREEN.blit(score_text, (250- score_text.get_width()//2, HEIGHT-110))
+        SCREEN.blit(level_text, (250- level_text.get_width()//2, HEIGHT-30))
+        pygame.display.update()
+        clock.tick(FPS)
+
+
+if __name__ == "__main__":
+    main()
+
+    
---
+++ variables.py
@@ -0,0 +1,30 @@
+import pygame
+import random
+import sys
+
+
+pygame.init()
+
+WIDTH, HEIGHT = 300, 500
+FPS = 35
+CELL = 20
+ROWS = (HEIGHT - 120) // CELL
+COLS = WIDTH // CELL
+
+
+BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+BG_COLOR = (31, 25, 76)
+GRID = (31, 25, 132)
+WIN = (50, 230, 50)
+LOSE = (252, 91, 122)
+
+font = pygame.font.SysFont("verdana",50)
+font2  = pygame.font.SysFont("verdana", 15)
+
+ASSETS ={
+    1: pygame.image.load("assets/1.png"),
+    2: pygame.image.load("assets/2.png"),
+    3: pygame.image.load("assets/3.png"),
+    4: pygame.image.load("assets/4.png")
+}
